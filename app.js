54aa63786e67a1a550540d52e008c5247b478161
Ursula Kesek <52711298+UrsulaKesek@users.noreply.github.com>
2020-02-26 18:35:32 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+let input = document.querySelector('.input_text');
+let main = document.querySelector('#name');
+let tempC = document.querySelector('.tempC');
+let tempF = document.querySelector('.tempF');
+let desc = document.querySelector('.desc');
+let button = document.querySelector('.submit');
+let icon = document.querySelector('.icon');
+let country = document.querySelector('.country');
+ 
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=8cee7f2d45cd8c8754ca127ff2455bab')
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  let iconValue = data.weather[0].icon;
+  let tempValueC = Math.round(parseFloat(data.main.temp)-273.15);
+  let tempValueF = Math.round(parseFloat(data.main.temp)-273.15) * 1.8 + 32;
+  let nameValue = data.name;
+  let countryValue = data.sys.country;
+  let descValue = data.weather[0].description;
+
+
+  icon.innerHTML = `<img src = "icons/${iconValue}.png"/>`;
+  desc.innerHTML = "Description:"+ " " +descValue;
+  tempC.innerHTML = "Temperature:"+ " " +tempValueC + "&#8451";
+  tempF.innerHTML = "in Fahrenheit:"+ " " +tempValueF + "&#8457";
+  main.innerHTML = nameValue;
+  country.innerHTML = "Country:"+ " " +countryValue;
+
+  input.value ="";
+
+})
+
+.catch(err => alert("No such city name!"));
+})
+
+
+ 
