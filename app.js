a9d2c708bb2a393dade2c4c8c8f848cbfaa7543c
Ursula Kesek <ursky@blueyonder.co.uk>
2020-02-28 23:31:32 +0000

    edited all three files

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const spinner = document.getElementById("spinner");
 let input = document.querySelector(".input_text");
 let main = document.querySelector("#name");
@@ -9,40 +8,41 @@ let button = document.querySelector(".submit");
 let icon = document.querySelector(".icon");
 let country = document.querySelector(".country");
 
+input.addEventListener("keyup", function() {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".submit").click();
 
-  input.addEventListener("keyup",function() {
-    if(event.keyCode === 13){
-      event.preventDefault();
-      document.querySelector(".submit").onclick();
-    
-  spinner.removeAttribute("hidden");
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      input.value +
-      "&appid=8cee7f2d45cd8c8754ca127ff2455bab"
-  )
-    .then(response => response.json())
-    .then(data => {
-      setTimeout(fetch,[Math.random() * 4500]);
-      spinner.setAttribute("hidden", "");
-      console.log(data);
-      let iconValue = data.weather[0].icon;
-      let tempValueC = Math.round(parseFloat(data.main.temp) - 273);
-      let tempValueF = Math.round(parseFloat(data.main.temp) - 273) * 1.8 + 32;
-      let nameValue = data.name;
-      let countryValue = data.sys.country;
-      let descValue = data.weather[0].description;
+    spinner.removeAttribute("hidden");
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        input.value +
+        "&appid=8cee7f2d45cd8c8754ca127ff2455bab"
+    )
+      .then(response => response.json())
+      .then(data => {
+        let min = 1000;
+        let max = 10000;
+        setTimeout(fetch, Math.random * (max - min + 1) + min);//max and min inclusive//
+        spinner.setAttribute("hidden", "");
+        console.log(data);
+        let iconValue = data.weather[0].icon;
+        let tempValueC = Math.round(parseFloat(data.main.temp) - 273);
+        let tempValueF =
+          Math.round(parseFloat(data.main.temp) - 273) * 1.8 + 32;
+        let nameValue = data.name;
+        let countryValue = data.sys.country;
+        let descValue = data.weather[0].description;
 
-      main.innerHTML = nameValue;
-      icon.innerHTML = `<img src = "icons/${iconValue}.png"/>`;
-      desc.innerHTML = "Description:" + " " + descValue;
-      tempC.innerHTML = "Temperature:" + " " + tempValueC + "&#8451";
-      tempF.innerHTML = "in Fahrenheit:" + " " + tempValueF + "&#8457";
-      country.innerHTML = "Country:" + " " + countryValue;
-      input.value = "";
-    })
-    
-    .catch(() => alert("No such city name!"));
+        main.innerHTML = nameValue;
+        icon.innerHTML = `<img src = "icons/${iconValue}.png"/>`;
+        desc.innerHTML = "Description:" + " " + descValue;
+        tempC.innerHTML = "Temperature:" + " " + tempValueC + "&#8451";
+        tempF.innerHTML = "in Fahrenheit:" + " " + tempValueF + "&#8457";
+        country.innerHTML = "Country:" + " " + countryValue;
+        input.value = "";
+      })
+
+      .catch(() => alert("No such city name!"));
   }
 });
-  
